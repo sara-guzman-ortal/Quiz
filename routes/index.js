8e36be32dece1fa5327b61936582b791eb27d11b
sara-guzman-ortal <sara.guzman.ortal@alumnos.upm.es>
2015-05-01 14:06:16 +0200

    Quiz 6 creditos

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,11 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+router.get('/author', function(req, res) {
+  res.render('author', {
+    author: 'Sara Guzmán Ortal',
+    photo: '<img src="/images/photo.jpg" width="100px" alt="Sara Guzman Ortal">'});
+});
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer',   quizController.answer);
