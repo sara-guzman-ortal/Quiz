4899ec3fe80c9b61493c21a1a51b85bb07b79918
sara-guzman-ortal <sara.guzman.ortal@alumnos.upm.es>
2015-05-05 21:11:54 +0200

    Quiz 9: Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,20 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
 router.get('/author', function(req, res) {
   res.render('author', {
     author: 'Sara Guzmán Ortal',
     photo: '<img src="/images/photo.jpg" width="100px" alt="Sara Guzman Ortal">'});
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
-
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
