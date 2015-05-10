628b637cb9b288b439deb6ef336e816d606330b1
sara-guzman-ortal <sara.guzman.ortal@alumnos.upm.es>
2015-05-10 18:16:13 +0200

    Buscador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,11 +14,32 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(
-    function(quizes) {
-      res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+  var cadena = "";
+    if (req.query.search === undefined) {
+        models.Quiz.findAll().then(function(quizes) {
+            res.render('quizes/index', {
+                quizes: quizes,
+                errors: []
+            });
+        }).catch(function(error) {next(error);
+        });
     }
-  ).catch(function(error){next(error)});
+    else
+    {
+        cadena = '%' + req.query.search + '%';
+        cadena = cadena.replace(/\s/g, '%');
+        models.Quiz.findAll( {
+            where: ['pregunta like ?', cadena],
+            order: ['pregunta']
+        }).then(function(quizes) {
+            res.render('quizes/index', {
+                quizes: quizes,
+                errors: []
+            });
+        }).catch(function(error) {
+            next(error);
+        });
+    }
 };
 
 // GET /quizes/:id
